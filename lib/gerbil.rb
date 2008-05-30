ec7a0b359518af2c01bc65656b4d60ecec74b832
Suraj N. Kurapati <sunaku@gmail.com>
2008-05-29 21:42:34 -0700

    add release info

--- lib/gerbil.rb
+++ lib/gerbil.rb
@@ -2,8 +2,8 @@
 
 Gerbil = {
   :name    => 'Gerbil',
-  :version => '2.0.0',
-  :release => '2008-02-03',
+  :version => '2.1.0',
+  :release => '2008-05-29',
   :website => 'http://gerbil.rubyforge.org',
   :home    => File.expand_path(File.join(File.dirname(__FILE__), '..'))
 }
