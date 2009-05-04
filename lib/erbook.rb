98f7ddc1b9ea89a6c4edf5abc030da89f7447822
Suraj N. Kurapati <sunaku@gmail.com>
2009-05-03 21:57:10 -0700

    add release notes for version 7.0.0

--- lib/erbook.rb
+++ lib/erbook.rb
@@ -9,11 +9,12 @@ require 'inochi'
 
 Inochi.init :ERBook,
   :program => 'erbook',
-  :version => '6.1.0',
-  :release => '2009-02-14',
+  :version => '7.0.0',
+  :release => '2009-05-03',
   :website => 'http://snk.tuxfamily.org/lib/erbook/',
   :tagline => 'Extensible document processor based on eRuby',
   :require => {
+    'ember'     => '~> 0',   # for eRuby template processing
     'maruku'    => '~> 0.5', # for Markdown to XHTML conversion
     'coderay'   => '>= 0.8', # for syntax coloring of source code
     'rainpress' => '~> 1',   # for minifying CSS
