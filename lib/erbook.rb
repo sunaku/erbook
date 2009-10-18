f0436454609a1fad59e96bea224e81a34243b023
Suraj N. Kurapati <sunaku@gmail.com>
2009-10-18 16:38:54 -0700

    Version 9.0.0

--- lib/erbook.rb
+++ lib/erbook.rb
@@ -9,8 +9,8 @@ require 'inochi'
 
 Inochi.init :ERBook,
   :program => 'erbook',
-  :version => '8.0.0',
-  :release => '2009-10-10',
+  :version => '9.0.0',
+  :release => '2009-10-18',
   :website => 'http://snk.tuxfamily.org/lib/erbook/',
   :tagline => 'Write books, manuals, and documents in eRuby',
   :require => {
